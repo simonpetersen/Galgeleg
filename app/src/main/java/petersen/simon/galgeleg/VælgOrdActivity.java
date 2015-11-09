414d7fa5c7ae189f0eaf797a7cf62d49a039a558
Simon <simon_petersen1994@hotmail.com>
2015-11-09 10:19:05 +0100

    Tilføjet ny activity.

---
+++ app/src/main/java/petersen/simon/galgeleg/VælgOrdActivity.java
@@ -0,0 +1,16 @@
+package petersen.simon.galgeleg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Simon on 09/11/15.
+ */
+public class VælgOrdActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView;
+    }
+}
